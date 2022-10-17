c310046ea8c33fc7a4cc9cf130b9f24ae13a28f5
Palmanius <hrdy@gsedu.sk>
2022-10-17 14:35:09 +0200

    Create Q01dFINISHED.py

---
+++ Q01dFINISHED.py
@@ -0,0 +1,14 @@
+# Q01d
+
+number1 = int(input("Enter the first number"))
+number2 = int(input("Enter the second number")) 
+
+if number1 == number2:
+    print("Numbers are equal")
+elif number1 < number2:
+    print("The highest number is", number2, "and the lowest number is", number1)
+else: 
+    print("The highest number is", number1, "and the lowest number is", number2)
+
+
+    
