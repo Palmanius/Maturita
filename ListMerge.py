ba95b412a380f40431b3f5be475e84e64e2f2f10
Palmanius <hrdy@gsedu.sk>
2022-11-23 12:42:32 +0100

    a

--- ListMerge.py
+++ ListMerge.py
@@ -9,17 +9,17 @@ class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:        
         def Merger(list1,list2,list3) -> ListNode:
             if list1 == None and list2 == None:
-                return ListNode(list3,None)
+                return None
             elif list1 == None:
                 return Merger(None,list2.next,ListNode(list3,list2.val))
             elif list2 == None:
-                return Merger(list1.next,None,ListNode(list3,list1.val))
+                return Merger(list1.next,None,ListNode(list3.next,list1.val))
             elif list1.val < list2.val:
-                return Merger(list1.next,list2,ListNode(list3,list1.val))
+                return Merger(list1.next,list2,ListNode(list3.next,list1.val))
             else:
-                return Merger(list1,list2.next,ListNode(list3,list2.val))
+                return Merger(list1,list2.next,ListNode(list3.next,list2.val))
 
-        return(Merger(list1,list2,None))
+        return(Merger(list1,list2,ListNode()))
 
 
         
