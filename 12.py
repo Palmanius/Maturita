478121373494d92246946c412fc1c5f2ee7af440
Palmanius <hrdy@gsedu.sk>
2023-02-06 13:28:13 +0100

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,6 +1,22 @@
-#task2
-A = input("First side of the triangle")
-B = input("Second side of the triangle")
-C =(A**2 + B**2)**(0.5)
+# #task2
+# A = input("First side of the triangle")
+# B = input("Second side of the triangle")
+# C = (int(A)**2 + int(B)**2)**(0.5)
 
-print("Third side of the triangle is: "+str(C))
+# print("Third side of the triangle is: "+str(C))
+
+#task3
+from math import factorial
+number = str(factorial(25))
+counter = 0
+for n in reversed(number):
+    if n =="0":
+        counter += 1
+    else:
+        break
+print(number)
+print(counter)
+
+
+
+
