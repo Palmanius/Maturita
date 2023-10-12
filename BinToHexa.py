33340defcac6a06df42b138f4d8b5f0f8700d440
Pavol Hrdy <Hrdy@gsedu.local>
2023-10-12 12:58:27 +0200

    Create BinToHexa.py

---
+++ BinToHexa.py
@@ -0,0 +1,26 @@
+Binary = "10110110"
+Hexa = ""
+ConversionTable = {
+    "0000":"0",
+    "0001":"1",
+    "0010":"2",
+    "0011":"3",
+    "0100":"4",
+    "0101":"5",
+    "0110":"6",
+    "0111":"7",
+    "1000":"8",
+    "1001":"9",
+    "1010":"A",
+    "1011":"B",
+    "1100":"C",
+    "1101":"D",
+    "1110":"E",
+    "1111":"F"
+}
+
+if len(Binary)%4 != 0: Binary = "0"*(4-len(Binary)%4) + Binary
+for i in range(0,len(Binary),4):
+    print(Binary[i:i+4])
+    Hexa += ConversionTable[Binary[i:i+4]]
+print(Hexa)
